0c1d15bee3ea81fe245ce2fadbc4b63244a35c6c
igor5544 <igor5544@bk.ru>
2019-09-13 21:45:25 +0500

    Добавил попапы

--- js/main.js
+++ js/main.js
@@ -12,10 +12,19 @@ $(function () {
         $('#adress-2', 'footer').trigger('click');
     })
 
+    $('.features__list li', '#features').on('click', function () {
+        $('a', this)[0].click();
+    });
+
+    $('#modal-features__item-5 a').on('click', function () {
+        $('.mfp-close').trigger('click');
+    });
+
+
+    // Краткое описание тренажёров
 
     $('.service__item', '#service').hover(function () {
         $('.service__item-text', this).slideToggle("slow").css('display', 'block');
-    
     });
     
     $('.popup-gallery').magnificPopup({
@@ -51,6 +60,11 @@ $(function () {
         speed: 200,
         });
     };
+
+    // Удалени лишней анимации на устройсвах
+    if ($(window).width() < 1000){
+        $('.form__form', '#form').removeClass('flipInX');
+    }
     
     $('#adress-2', 'footer').on('click', function () {
         $('#map', 'footer').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d647.9779547870723!2d60.61588626938847!3d56.88831242548529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16df412adf3ff%3A0xd0320297ca7410c1!2z0YPQuy4g0JHQsNGD0LzQsNC90LAsIDUsINCV0LrQsNGC0LXRgNC40L3QsdGD0YDQsywg0KHQstC10YDQtNC70L7QstGB0LrQsNGPINC-0LHQuy4sIDYyMDAxNw!5e0!3m2!1sru!2sru!4v1567855396575!5m2!1sru!2sru');
@@ -75,6 +89,12 @@ $(function () {
         $('a', this)[0].click();
     });
 
+    $('.popup-modal').magnificPopup({
+        type: 'inline',
+        preloader: false
+    });
+
+    
 
 });
 
